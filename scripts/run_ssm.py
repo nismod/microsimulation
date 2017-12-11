e5d5de44336b741e384352f8f623a2e5c3649140
virgesmith <a.p.smith@leeds.ac.uk>
2017-12-11 17:09:17 +0000

    move files + add run script

---
+++ scripts/run_ssm.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# run script for Household microsynthesis
+
+import sys
+import time
+#import humanleague
+#import ukcensusapi.Nomisweb as Api
+import microsimulation.static as Static
+import household_microsynth.utils as Utils
+
+#assert humanleague.version() > 1
+CACHE_DIR = "./cache"
+
+# The microsynthesis makes use of the following tables:
+def main(region, resolution):
+
+  # # start timing
+  start_time = time.time()
+
+  print("Microsynthesis region:", region)
+  print("Microsynthesis resolution:", resolution)
+  # init microsynthesis
+  try:
+    msynth = Microsynthesiser.Microsynthesis(region, resolution, CACHE_DIR)
+  except Exception as e:
+    print(e)
+    return
+
+
+
+  # generate the population
+  try:
+    msynth.run()
+  except Exception as e:
+    print(e)
+    return
+
+  print("Done. Exec time(s): ", time.time() - start_time)
+
+
+if __name__ == "__main__":
+  if len(sys.argv) != 3:
+    print("usage:", sys.argv[0], "<region> <resolution>")
+    print("e.g:", sys.argv[0], "E09000001 OA11")
+  else:
+    REGION = sys.argv[1]
+    RESOLUTION = sys.argv[2]
+    main(REGION, RESOLUTION)
