46b3eb10188dc522a49f4aea6f94002958e7c80b
virgesmith <a.p.smith@leeds.ac.uk>
2018-03-14 21:12:58 +0000

    progress on assignment

--- microsimulation/assignment.py
+++ microsimulation/assignment.py
@@ -28,12 +28,14 @@ class Assignment:
 
     self.p_data = pd.read_csv(p_file, index_col="PID")
 
+    self.p_data["HID"] = pd.Series(-1, self.p_data.index)
+    self.h_data["FILLED"] = pd.Series(False, self.p_data.index)
+
     #print(h_data.head())
     #print(p_data.head())
 
     # get OA<->MSOA mapping
     self.geog_lookup = pd.read_csv("../../Mistral/persistent_data/oa2011codes.csv")
-
 
   def run(self):
     """
@@ -41,17 +43,16 @@ class Assignment:
     """
 
     eths = self.h_data.LC4202EW_C_ETHHUK11.unique()
-    print(eths)
+    #eths = [eths[1]]
+    #print(eths)
     # we have different eth resolution in the datasets
     eth_mapping = { -1:-1, 2:2, 3:3, 4:4, 5:4, 7:5, 8:5, 9:5, 10:5, 12:6, 13:6, 14:6, 15:6, 16:6, 18:7, 19:7, 20:7, 22:8, 23:8 }
 #    self.p_data.replace({"DC2101EW_C_ETHPUK11": eth_mapping})
     self.p_data.DC2101EW_C_ETHPUK11.replace(eth_mapping, inplace=True)
-    print(self.p_data.DC2101EW_C_ETHPUK11.unique())
+    #print(self.p_data.DC2101EW_C_ETHPUK11.unique())
     #assert len(eths) == len(self.p_data.DC2101EW_C_ETHPUK11.unique())
 
-
-    self.p_data["HID"] = pd.Series(-1, self.p_data.index)
-    self.h_data["FILLED"] = pd.Series(False, self.p_data.index)
+    self.__stats()
 
     msoas = self.p_data.Area.unique()
 
@@ -70,46 +71,26 @@ class Assignment:
 
         # assign persons to houses
         p_sample = np.random.choice(p_ref, len(h_ref), replace=False)
+        # mark single-occupant houses as filled
+        h1_ref = self.h_data[(self.h_data.Area.isin(oas)) & (self.h_data.LC4202EW_C_ETHHUK11 == eth) & (self.h_data.LC4408_C_AHTHUK11 == 1)].index
+        self.h_data.loc[h1_ref, "FILLED"] = True
 
         self.p_data.loc[p_sample, "HID"] = h_ref
 
-        #print(len(h_ref))
-        #print(len(p_ref))
-        #print(len(p_sample))
+        print(len(h_ref))
+        print(len(p_ref))
+        print(len(p_sample))
 
-      # mark single-occupant houses as filled
-      h_ref = self.h_data[(self.h_data.Area.isin(oas)) & (self.h_data.LC4408_C_AHTHUK11 == 1)].index
-      self.h_data.loc[h_ref, "FILLED"] = True
-
-      print("P:", len(self.p_data[self.p_data.HID>0]) / len(self.p_data))
-      print("H:", len(self.h_data[self.h_data.FILLED]) / len(self.h_data))
-      print("P rem:", len(self.p_data[self.p_data.HID==-1]))
-      print("H rem:", len(self.h_data[self.h_data.FILLED==False]))
+        self.__stats()
 
       self.p_data.to_csv("pass.csv")
       self.h_data.to_csv("hass.csv")
 
-    # for (msoa in msoas) {
-    #   oas = lookup[lookup$msoa==msoa,]$oa
-
-    #   hrp = p[Area == msoa & DC1117EW_C_AGE > 16]
-      
-    #   # dont consider unoccupied dwellings yet
-    #   oh = h[h$Area %in% oas & h$LC4408_C_AHTHUK11>0,]
-    #   nh = nrow(oh)
-      
-    #   # sample HRPs
-    #   s = hrp[sample(nrow(hrp), nh, replace=F),]
-      
-    #   for (i in 1:nrow(s)) {
-    #     p[p$PID==s[i,]$PID,]$HID = oh[i,]$HID
-    #   }
-      
-    #   # single-occupant households 
-    #   #h1 = sample(h[Area %in% oas], nh
-    # }
-
- #   print(self.p_data.head())
+  def __stats(self):
+    print("P:", len(self.p_data[self.p_data.HID>0]) / len(self.p_data))
+    print("H:", len(self.h_data[self.h_data.FILLED]) / len(self.h_data))
+    print("P rem:", len(self.p_data[self.p_data.HID==-1]))
+    print("H rem:", len(self.h_data[self.h_data.FILLED==False]), "(", len(self.h_data[self.h_data.LC4408_C_AHTHUK11==-1]), ")")
 
 
 # Selected household ethnicities
