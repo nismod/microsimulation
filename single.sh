b955f15d0fb8052aaee5a562bab90cecee062a77
virgesmith <a.p.smith@leeds.ac.uk>
2018-07-27 15:36:31 +0100

    update hpc scripts

--- single.sh
+++ single.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 
-# single job submission - args from cmd line
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <region(s)> <resolution>"
-  echo   e.g. $0 E09000001 MSOA11
+# single job submission - args from cmd line TODO config file
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <region>"
+  echo   e.g. $0 E09000001
   exit 1
 fi
 
 region=$1
-resolution=$2
 
-if [ ! -f ./apikey.sh ]; then
+if [ ! -f ~/apikey.sh ]; then
   echo "api key not found. Please specify your Nomisweb API key in ./apikey.sh, e.g.:"
   echo "export NOMIS_API_KEY=0x0123456789abcdef0123456789abcdef01234567"
   exit 1
 fi
-. ./apikey.sh
+. ~/apikey.sh
 
-source activate default
+# TODO check conda
 
 qsub_params="-l h_rt=2:0:0"
 
-outfile="hh_"$region"_"$resolution".csv"
-if [ ! -f $outfile ]; then
-  export REGION=$region
-  echo Submitting job for $REGION
-  qsub $qsub_params run.sh
-else
-  echo $region done, not resubmitting
-fi
+export REGION=$region
+echo Submitting job for $REGION
+qsub $qsub_params run.sh
 
