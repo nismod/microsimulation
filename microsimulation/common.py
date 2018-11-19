5d95748c56802a60bc94e0147a36c3311b32dc31
virgesmith <a.p.smith@leeds.ac.uk>
2018-11-19 17:50:46 +0000

    seed the microsynth using LAD-level crosstabulations

--- microsimulation/common.py
+++ microsimulation/common.py
@@ -31,7 +31,8 @@ class Base(object):
 
   def __get_census_data_sc(self):
 
-    print("Synthesising Scottish DC1117/DC2101 tables from univariate data")
+    print("Synthesising Scottish DC1117/DC2101 tables from LAD-level seeds and univariate data")
+
     # age only, no gender
     qs103sc = self.data_api_sc.get_data("QS103SC", self.region, self.resolution, category_filters={"QS103SC_0_CODE": range(1,102)})
     qs103sc = utils.cap_value(qs103sc, "QS103SC_0_CODE", 86, "OBS_VALUE")
@@ -42,11 +43,20 @@ class Base(object):
     nages = len(qs103sc.QS103SC_0_CODE.unique())
     nsexes = 2
 
+    # Get a LAD-level seed population by age and gender
+    dc1117lad = self.data_api_sc.get_data("DC1117SC", self.region, "LAD", category_filters={"DC1117SC_0_CODE": [1,2], "DC1117SC_1_CODE": range(1,102)})
+    dc1117lad = utils.cap_value(dc1117lad, "DC1117SC_1_CODE", 86, "OBS_VALUE")
+    dc1117seed = utils.unlistify(dc1117lad, ["DC1117SC_0_CODE", "DC1117SC_1_CODE"], [2, 86], "OBS_VALUE").astype(float)
+    # expand to all geogs within LAD
+    dc1117seed = np.dstack([dc1117seed] * ngeogs).T
+
     ga = utils.unlistify(qs103sc, ["GEOGRAPHY_CODE", "QS103SC_0_CODE"], [ngeogs, nages], "OBS_VALUE")
     gs = utils.unlistify(qs104sc, ["GEOGRAPHY_CODE", "QS104SC_0_CODE"], [ngeogs, nsexes], "OBS_VALUE")
-    # TODO use a LAD-level seed population
-    msynth = hl.qis([np.array([0,1]), np.array([0,2])], [ga,gs])
+    msynth = hl.qisi(dc1117seed, [np.array([0,1]), np.array([0,2])], [ga,gs])
+    #msynth = hl.qis([np.array([0,1]), np.array([0,2])], [ga,gs])
     utils.check_result(msynth)
+    # TODO pending humanleague seed consistency check
+    assert dc1117seed.shape == msynth["result"].shape
 
     dc1117sc = utils.listify(msynth["result"], "OBS_VALUE", ["GEOGRAPHY_CODE", "C_AGE", "C_SEX"])
     dc1117sc.GEOGRAPHY_CODE = utils.remap(dc1117sc.GEOGRAPHY_CODE, qs103sc.GEOGRAPHY_CODE.unique())
@@ -54,15 +64,24 @@ class Base(object):
     dc1117sc.C_SEX = utils.remap(dc1117sc.C_SEX, [1, 2])
     #print(dc1117sc.head())
 
-    # These ETH codes are slightly different to E&W codes
+    # These ETH codes are slightly different to E&W codes...
     # ETH Totals = [1,8,9,15,18,22]
-    ks201sc = self.data_api_sc.get_data("KS201SC", self.region, self.resolution, category_filters={"KS201SC_0_CODE": [2,3,4,5,6,7,8,10,11,12,13,14,16,17,19,20,21,23,24]})
+    eths = [2,3,4,5,6,7,8,10,11,12,13,14,16,17,19,20,21,23,24]
+    ks201sc = self.data_api_sc.get_data("KS201SC", self.region, self.resolution, category_filters={"KS201SC_0_CODE": eths})
     neths = len(ks201sc.KS201SC_0_CODE.unique())
+
+    # Get a LAD-level seed population by age and gender
+    dc2101lad = self.data_api_sc.get_data("DC2101SC", self.region, "LAD", category_filters={"DC2101SC_0_CODE": eths, "DC2101SC_1_CODE": [1,2], "DC2101SC_2_CODE": 0})
+    dc2101seed = utils.unlistify(dc2101lad, ["DC2101SC_1_CODE", "DC2101SC_0_CODE"], [2, neths], "OBS_VALUE").astype(float)
+    # expand to all geogs within LAD
+    dc2101seed = np.dstack([dc2101seed] * ngeogs).T
+
     #print(ks201sc.head())
     ge = utils.unlistify(ks201sc, ["GEOGRAPHY_CODE", "KS201SC_0_CODE"], [ngeogs, neths], "OBS_VALUE")
     # TODO use a LAD-level seed population
-    msynth = hl.qis([np.array([0,1]), np.array([0,2])], [ge,gs])
+    msynth = hl.qisi(dc2101seed, [np.array([0,1]), np.array([0,2])], [ge,gs])
     utils.check_result(msynth)
+    assert dc2101seed.shape == msynth["result"].shape
 
     dc2101sc = utils.listify(msynth["result"], "OBS_VALUE", ["GEOGRAPHY_CODE", "C_ETHPUK11", "C_SEX"])
     dc2101sc.GEOGRAPHY_CODE = utils.remap(dc2101sc.GEOGRAPHY_CODE, qs103sc.GEOGRAPHY_CODE.unique())
